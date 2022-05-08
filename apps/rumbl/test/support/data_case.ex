8239ef8ce1cf0ea6e77c731f700fc31511a80369
Hazuli Fidastian <hazulifidastian@gmail.com>
2022-05-09 04:05:46 +0700

    Copy from old rumbl

--- apps/rumbl/test/support/data_case.ex
+++ apps/rumbl/test/support/data_case.ex
@@ -24,6 +24,7 @@ defmodule Rumbl.DataCase do
       import Ecto.Changeset
       import Ecto.Query
       import Rumbl.DataCase
+      import Rumbl.TestHelpers
     end
   end
 
@@ -49,10 +50,19 @@ defmodule Rumbl.DataCase do
 
   """
   def errors_on(changeset) do
-    Ecto.Changeset.traverse_errors(changeset, fn {message, opts} ->
-      Regex.replace(~r"%{(\w+)}", message, fn _, key ->
-        opts |> Keyword.get(String.to_existing_atom(key), key) |> to_string()
-      end)
-    end)
+    Ecto.Changeset.traverse_errors(
+      changeset,
+      fn {message, opts} ->
+        Regex.replace(
+          ~r"%{(\w+)}",
+          message,
+          fn _, key ->
+            opts
+            |> Keyword.get(String.to_existing_atom(key), key)
+            |> to_string()
+          end
+        )
+      end
+    )
   end
 end
